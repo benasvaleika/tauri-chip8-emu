5297d2149ea0dd478dc12db88459050602499af0
benasvaleika <bentini113@gmail.com>
2022-05-08 15:46:52 +0300

    add: File dialog, load rom to cpu ram

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,17 +2,29 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+#![allow(non_snake_case)]
 
 mod cpu;
+
+use std::fs;
 
 use cpu::CPU;
 use tauri::Window;
 
-fn main() {
-    let cpu = CPU::new();
+#[tauri::command]
+fn test_cpu(window: Window, rom_path: String) {
+    println!("test_cpu invoked");
 
+    let rom_contents = fs::read(rom_path).expect("Error occured while reading the file");
+
+    let mut cpu = CPU::new();
+
+    cpu.load_rom(&rom_contents);
+}
+
+fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![test_cpu])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { invoke } from "@tauri-apps/api";
+import { open } from "@tauri-apps/api/dialog";
+import { Button } from "./components/Button";
+
 function App() {
-  return <div></div>;
+  const testHandler = async () => {
+    const file_path = await open();
+    console.log(file_path);
+    invoke("test_cpu", { romPath: file_path });
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="mt-10 text-4xl text-white font-extrabold">
+        CHIP-8 EMULATOR
+      </h1>
+      <Button text="Test" onClick={testHandler} className="mt-10 mr-4" />
+    </div>
+  );
 }
 
 export default App;
