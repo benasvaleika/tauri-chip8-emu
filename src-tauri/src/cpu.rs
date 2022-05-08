00fa71dd28b964813d60ba6d754c24f0cead20cb
benasvaleika <bentini113@gmail.com>
2022-05-08 16:47:30 +0300

    add: chip8 font now loads into memory

--- src-tauri/src/cpu.rs
+++ src-tauri/src/cpu.rs
@@ -1,6 +1,10 @@
+use crate::chip8_font::CHIP8_FONT_SET;
+
 pub struct CPU {
+    // pub for debuging
     pub ram: [u8; 4096],
-    vx: [u8; 16],
+    // pub for debuging
+    pub vx: [u8; 16],
     stack: [u16; 16],
     pc: usize,
     i: u16,
@@ -9,16 +13,29 @@ pub struct CPU {
 
 impl CPU {
     pub fn new() -> CPU {
-        // TODO: Load Font into ram
+        // Load font into ram
+        let mut ram_w_font = [0; 4096];
+        for (i, &byte) in CHIP8_FONT_SET.iter().enumerate() {
+            let curr_addr = 0x50 + i;
+            ram_w_font[curr_addr] = byte;
+        }
 
         CPU {
-            ram: [0; 4096],
+            ram: ram_w_font,
             vx: [0; 16],
             stack: [0; 16],
             pc: 0x200,
             i: 0,
             sp: 0,
         }
+    }
+
+    pub fn emulate_cycle(&mut self) {
+        let opcode = self.read_opcode();
+
+        self.pc += 2;
+
+        println!("{:0x?}", &opcode);
     }
 
     fn read_opcode(&self) -> u16 {
